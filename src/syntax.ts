9b0b1108559db03f4e615b60a4e6db69a3caec66
Jaden Geller <jaden.geller@gmail.com>
2017-06-08 02:21:34 -0700

    Add syntax iteration

---
+++ src/syntax.ts
@@ -0,0 +1,33 @@
+import { traverse, LetIn, Lambda, Variable, Application } from "./parse";
+import { Module } from "./compile";
+import { Interval } from "./utilities"
+
+enum HighlightType {
+    Bold = 1,
+    Underline = 2
+}
+
+export class Highlight {
+    constructor(
+        public region: Interval,
+        public type: HighlightType)
+    { }
+}
+
+// TODO: Consider imported functions
+export function* highlightRegions(module: Module): IterableIterator<Highlight> {
+    if (!module.program) { return };
+    for (const expr of traverse(module.program)) {
+        if (expr instanceof LetIn) {
+            // no-op
+        } else if (expr instanceof Lambda) {
+            const startIndex = expr.keywordToken.startIndex();
+            const endIndex = expr.variableToken ? expr.variableToken.endIndex() : expr.keywordToken.endIndex();
+            yield new Highlight(new Interval(startIndex, endIndex), HighlightType.Underline);
+        } else if (expr instanceof Variable) {
+            yield new Highlight(expr.token.substring.interval, HighlightType.Bold);
+        } else if (expr instanceof Application) {
+            // no-op
+        }
+    }
+}
