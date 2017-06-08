fa692fb66a4f9a6a396c07d1b6b2cb0cbc70e77d
Jaden Geller <jaden.geller@gmail.com>
2017-06-07 22:01:38 -0700

    Added parser

---
+++ src/parse.ts
@@ -0,0 +1,153 @@
+import { TokenGroup, TokenTree } from "./group";
+import { Token, WordToken, OpenerToken, CloserToken } from "./tokenize";
+import { PeekableIterator } from "./utilities";
+
+interface Binder {
+    variableToken: WordToken;
+}
+
+export class Variable {
+    constructor(
+        public binder: Partial<Binder>,
+        public token: WordToken
+    ) { }
+
+    public toString = (): string => {
+        if (this.binder.variableToken) {
+            return this.binder.variableToken.toNormalized();
+        } else {
+            return "_";
+        }
+    }
+}
+
+export class Lambda implements Partial<Binder> {
+    constructor(
+        public keywordToken: WordToken,
+        public variableToken?: WordToken
+    ) { }
+    public body?: Expression;
+
+    public toString = (): string => {
+        const name = this.variableToken ? this.variableToken.toNormalized() : "_";
+        const body = this.body ? this.body.toString() : "_";
+        return `λ${name}.${body}`;
+    }
+}
+
+export class Application {
+    constructor(
+        public lambda: Expression,
+        public argument: Expression
+    ) { }
+
+    public toString = (): string => {
+        return `(${this.lambda.toString()}) (${this.argument.toString()})`;
+    }
+}
+
+export class LetIn implements Partial<Binder> {
+    constructor(
+        public variableToken?: WordToken,
+        public variableValue?: Expression,
+        public body?: Expression | LetIn
+    ) { }
+
+    public toString = (): string => {
+        const name = this.variableToken ? this.variableToken.toNormalized() : "_";
+        const value = this.variableValue ? this.variableValue.toString() : "_";
+        const body = this.body ? this.body.toString() : "_";
+        return `let ${name} = ${value};\n${body}`;
+    }
+}
+
+export type Expression = Variable | Lambda | Application;
+export type Statement = Expression | LetIn;
+
+class Scope {
+    private base: { [key: string]: Binder[] } = {};
+
+    push(binder: Binder) {
+        const name = binder.variableToken.toNormalized();
+        if (!(name in this.base)) {
+            this.base[name] = []
+        }
+        this.base[name].unshift(binder)
+    }
+    pop(binder: Binder) {
+        const name = binder.variableToken.toNormalized();
+        if (name in this.base) {
+            this.base[name].shift()
+            if (this.base[name].length == 0) {
+                delete this.base[name];
+            }
+        }
+    }
+    lookup(name: string): Binder | null {
+        if (name in this.base) {
+            return this.base[name][0]
+        } else {
+            return null
+        }
+    }
+}
+
+export function parse(
+    keyword: string,
+    groups: TokenTree[],
+    globals: { [key: string]: LetIn } = {}
+): Expression | undefined {
+    let scope = new Scope();
+
+    function parseGroups(tokens: TokenTree[]): Expression | undefined {
+        let result: Expression | undefined;
+        function append(expr: Expression): Expression {
+            if (!result) {
+                result = expr;
+            } else {
+                result = new Application(result, expr);
+            }
+            return result;
+        }
+
+        let tokenIter = new PeekableIterator(tokens[Symbol.iterator]());
+        for (let token of tokenIter) {
+            if (token instanceof TokenGroup) {
+                const group = parseGroups(token.groupedTokens);
+                // Ignore empty groups
+                if (group) {
+                    append(group);
+                }
+            } else if (token instanceof Token) {
+                if (token.toNormalized() == keyword) {
+                    // Determine binding name
+                    let { value: nextToken, done } = tokenIter.peek();
+                    let variableToken: WordToken | undefined;
+                    if (!done && nextToken instanceof WordToken) {
+                        variableToken = nextToken;
+                        tokenIter.next();
+                    }
+
+                    // Parse body with binding in scope
+                    const lambda = new Lambda(token, variableToken)
+                    if (lambda.variableToken) { scope.push(<Binder>lambda); }
+                    const body = parseGroups(Array.from(tokenIter));
+                    if (lambda.variableToken) { scope.pop(<Binder>lambda); }
+                    lambda.body = body;
+                    // Return lambda node
+                    return append(lambda);
+                }
+                else {
+                    const local = scope.lookup(token.toNormalized());
+                    if (local) {
+                        append(new Variable(local, token));
+                    } else if (token.toNormalized() in globals) { // FIXME: Lowercasing is gross here.
+                        append(new Variable(globals[token.toNormalized()], token));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    return parseGroups(groups);
+}
