f388532e52c3460d282a1f1811e8780a1410a7eb
Jaden Geller <jaden.geller@gmail.com>
2017-06-08 02:30:19 -0700

    Switch to strict evaluation

--- src/codegen.ts
+++ src/codegen.ts
@@ -1,20 +1,19 @@
 import { Statement, LetIn, Variable, Lambda, Application } from "./parse";
 import { AssertionError } from "./utilities";
 
-// TODO: Handle exports/imports!
-function codegen(expr: Statement): string | null { // with lazy semantics!
+export function codegen(expr: Statement): string | null {
     if (expr instanceof Lambda) {
         if (!expr.variableToken || !expr.body) { return null; }
         const body = codegen(expr.body);
         if (!body) { return null; }
-        return "() => " + expr.variableToken.toNormalized() + " => " + body;
+        return `${expr.variableToken.toNormalized()} => ${body}`;
     } else if (expr instanceof Variable) {
         return expr.token.toNormalized();
     } else if (expr instanceof Application) {
         const func = codegen(expr.lambda);
         const arg = codegen(expr.argument);
         if (!func || !arg) { return null; }
-        return `() => this._ ? this._ : this._ = (${func})()(${arg})`; // Memoized!
+        return `(${func})(${arg})`; // Memoized!
     } else if (expr instanceof LetIn) {
         if (!expr.variableToken || !expr.variableValue) { return null; }
         const varValue = codegen(expr.variableValue);
